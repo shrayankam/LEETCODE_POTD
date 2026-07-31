1f018db222aa5ed9412240543bd5baccbad2fae3
Shrayanka Mallick <143018801+shrayankam@users.noreply.github.com>
2026-07-31 11:57:52 +0530

    Time: 70 ms (5.49%) | Memory: 48 MB (73.70%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,79 +1,30 @@
-// class Solution {
-//     public int minimumPushes(String word) {
-//         HashMap<Character, Integer> hm=new HashMap<>();
-//         int totalSum=0;
-//         for(int i=0;i<word.length();i++){
-//             char key=word.charAt(i);
-//             hm.put(key, hm.getOrDefault(key, 0) + 1);
-//         }
- 
-
-// Map<Character, Integer> sortedMap = hm.entrySet()
-//             .stream()
-//             .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
-//             .collect(Collectors.toMap(
-//                 Map.Entry::getKey, 
-//                 Map.Entry::getValue, 
-//                 (e1, e2) -> e1, 
-//                 LinkedHashMap::new // Maintains the descending order
-//             ));
-
-//         // 2. Print the sorted letters and frequencies
-
-
-//         int i=0;
-//         for(Integer value:sortedMap.values()){
-//             if(i<8){
-//                 totalSum+=value;
-//                 i++;
-//             }
-//             else if(i>=8 && i<16){
-//                 totalSum+=(value*2);
-//                 i++;
-//             }else if(i>=16 && i<24){
-//                 totalSum+=(value*3);
-//                 i++;
-//             }else{
-//                 totalSum+=(value*4);
-//                 i++;
-//             }
-//         }
-
-// //         System.out.println(hm);
-//         System.out.println(sortedMap); 
-//         return totalSum;
-//     }
-// }
-
-
 class Solution {
     public int minimumPushes(String word) {
         HashMap<Character, Integer> hm = new HashMap<>();
 
-for (char ch : word.toCharArray()) {
-    hm.put(ch, hm.getOrDefault(ch, 0) + 1);
-}
+        for (char ch : word.toCharArray()) {
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
 
-List<Integer> list = new ArrayList<>(hm.values());
-Collections.sort(list, Collections.reverseOrder());
+        List<Integer> list = new ArrayList<>(hm.values());
+        Collections.sort(list, Collections.reverseOrder());
 
-int ans = 0;
-int i = 0;
+        int ans = 0;
+        int i = 0;
 
-for (int value : list) {
-    if (i < 8)
-        ans += value;
-    else if (i < 16)
-        ans += value * 2;
-    else if (i < 24)
-        ans += value * 3;
-    else
-        ans += value * 4;
-    i++;
-}
+        for (int value : list) {
+            if (i < 8) {
+                ans += value;
+            } else if (i < 16) {
+                ans += value * 2;
+            } else if (i < 24) {
+                ans += value * 3;
+            } else {
+                ans += value * 4;
+            }
+            i++;
+        }
 
-return ans;
-
-
+        return ans;
     }
 }
