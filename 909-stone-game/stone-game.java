902da02b7b478403f887b598cbb7d79e6878341f
Shrayanka Mallick <143018801+shrayankam@users.noreply.github.com>
2026-08-02 12:17:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (62.03%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,22 +1,49 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int n = piles.length;
-        int[][] dp = new int[n][n];
+        // int n = piles.length;
+        // int[][] dp = new int[n][n];
 
-        for (int i = 0; i < n; i++)
-            dp[i][i] = piles[i];
+        // for (int i = 0; i < n; i++)
+        //     dp[i][i] = piles[i];
 
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
+        // for (int len = 2; len <= n; len++) {
+        //     for (int i = 0; i + len - 1 < n; i++) {
+        //         int j = i + len - 1;
 
-                int takeLeft = piles[i] - dp[i + 1][j];
-                int takeRight = piles[j] - dp[i][j - 1];
+        //         int takeLeft = piles[i] - dp[i + 1][j];
+        //         int takeRight = piles[j] - dp[i][j - 1];
 
-                dp[i][j] = Math.max(takeLeft, takeRight);
+        //         dp[i][j] = Math.max(takeLeft, takeRight);
+        //     }
+        // }
+
+        // return dp[0][n - 1] >= 0;
+
+        int l=0,r=piles.length-1,AliceS=0,BobS=0,Amax=0,Bmax=0;
+        boolean Alice=true, Bob=false;
+
+        while(l<r){
+            if(Alice){
+                Amax=Math.max(piles[l],piles[r]);
+                AliceS+=Amax;
+                if(AliceS-Amax==piles[l]){
+                    l++;
+                }else{
+                    r--;
+                }
+            }else{
+                Bmax=Math.max(piles[l],piles[r]);
+                BobS+=Bmax;
+                if(BobS-Bmax==piles[l]){
+                    l++;
+                }else{
+                    r--;
+                }
             }
         }
-
-        return dp[0][n - 1] >= 0;
+        if(AliceS>BobS){
+            return true;
+        }
+        return false;
     }
 }
