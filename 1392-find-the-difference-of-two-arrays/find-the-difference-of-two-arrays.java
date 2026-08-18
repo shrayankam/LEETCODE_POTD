83a5e780f0c4f33f26cdb5088fdacb217ae48531
Shrayanka Mallick <143018801+shrayankam@users.noreply.github.com>
2026-08-19 00:39:09 +0530

    Time: 13 ms (21.99%) | Memory: 46.4 MB (99.76%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> a1=new ArrayList<>();
+        // List<Integer> a1=new ArrayList<>();
         Set<Integer> s1 = new HashSet<>();
-        List<Integer> a2=new ArrayList<>();
+        // List<Integer> a2=new ArrayList<>();
         Set<Integer> s2 = new HashSet<>();
 
 
@@ -16,20 +16,29 @@ class Solution {
         }
 
 
-        for(int i=0;i<nums1.length;i++){
-            if(!(s2.contains(nums1[i])) && !(a1.contains(nums1[i]))){
-                a1.add(nums1[i]);
-            }
-        }
+        // for(int i=0;i<nums1.length;i++){
+        //     if(!(s2.contains(nums1[i])) && !(a1.contains(nums1[i]))){
+        //         a1.add(nums1[i]);
+        //     }
+        // }
 
-        for(int i=0;i<nums2.length;i++){
-            if(!(s1.contains(nums2[i])) && !(a2.contains(nums2[i]))){
-                a2.add(nums2[i]);
-            }
-        }
-        
-        ans.add(a1);
-        ans.add(a2);
+        // for(int i=0;i<nums2.length;i++){
+        //     if(!(s1.contains(nums2[i])) && !(a2.contains(nums2[i]))){
+        //         a2.add(nums2[i]);
+        //     }
+        // }
+
+        Set<Integer> temp=new HashSet<>(s1);
+        s1.removeAll(s2);
+        s2.removeAll(temp);
+        // CHANGE THIS:
+// ans.add(s1);
+// ans.add(s2);
+
+// TO THIS:
+ans.add(new ArrayList<>(s1));
+ans.add(new ArrayList<>(s2));
+
         return ans;
     }
 }
