dc9c928547153397078e5e2a11f3490ab8a2c36f
Shrayanka Mallick <143018801+shrayankam@users.noreply.github.com>
2026-09-24 13:19:28 +0530

    Time: 1 ms (99.69%) | Memory: 45.7 MB (40.74%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.java
@@ -1,40 +1,18 @@
-// class Solution {
-//     public int smallestIndex(int[] nums) {
-//         for(int i=0;i<nums.length;i++){
-//             int a=nums[i];
-//             int sum=0;
-//             while(a>0){
-//                 int rem=a%10;
-//                 sum+=rem;
-//                 a/=10;
-//             }
-
-//             if(sum==i){
-//                 return i;
-//             }
-//         } 
-//         return -1;
-//     }
-// }
-
 class Solution {
     public int smallestIndex(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-
-        int n = nums[i];
-            int sum = 0;
-
-            while (n > 0) {
-                int digit = n % 10;
-                sum = sum + digit;
-                n = n / 10;
+        for(int i=0;i<nums.length;i++){
+            int n=nums[i];
+            int sum=0;
+            while(n>0){
+                int rem=n%10;
+                sum+=rem;
+                n/=10;
             }
 
-            if (sum == i) {
+            if(sum==i){
                 return i;
             }
-        
-        }
+        } 
         return -1;
     }
 }
